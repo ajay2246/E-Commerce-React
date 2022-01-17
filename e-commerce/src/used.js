4a827196006e9bd3c0781ea9fc94b2a37bfe2409
ajay <ajaykancheti99@gmail.com>
2022-01-17 20:19:39 +0530

    code

---
+++ e-commerce/src/used.js
@@ -0,0 +1,25 @@
+
+
+function Used(){
+    return(
+        <div style={{maxWidth:"400px",margin:"auto",backgroundColor:"white"}}>
+      <div style={{height:"300px",maxWidth:"400px",margin:"auto",backgroundColor:"black",color:"white",marginTop:"10px",position:"relative"}}>
+        <div style={{textAlign:"center",fontSize:"30px",paddingTop:"20px"}}>man</div>
+        <div style={{textAlign:"center",fontSize:"30px",paddingLeft:"70px",marginTop:"-15px"}}>matters</div>
+        <div style={{color:"pink",textAlign:"center",fontSize:"28px",marginTop:"30px"}}>Congratulations <img src="https://cdn-icons.flaticon.com/png/128/2274/premium/2274543.png?token=exp=1641393289~hmac=42a46c313a58dff58cae3bd1410d6425" alt="img" height="25px"/></div>
+      </div>
+      <div style={{height:"200px",width:"340px",textAlign:"center",position:"absolute",top:"160px",left:"600px",backgroundColor:"white",marginTop:"20px",borderRadius:"25px",boxShadow:" 2px 5px #888888"}}>
+        <div style={{marginTop:"30px",color:"blue",marginBottom:"30px",fontSize:"25px"}}>pay<span style={{color:"orange"}}>tm</span></div>
+        <div style={{fontSize:"25px",color:"pink"}}>Gift Card</div>
+        <div style={{fontSize:"25px",color:"pink"}}>Of</div>
+        <div style={{fontSize:"35px",color:"blue"}} >Rs 200</div>
+      </div>
+      <div style={{textAlign:"center",fontSize:"20px",marginTop:"85px"}}>You have WON Paytm Gift<br/>Card of Rs 200 for <span style={{fontWeight:"bold"}}>placing the order<br/>on Man Matters</span></div>
+      <div style={{marginTop:"30px",fontSize:"30px",fontWeight:"bold",textAlign:"center",color:"blue",paddingBottom:"50px"}}>
+          THANK YOU <br/>
+         <span style={{color:"green"}}>You have already used <br/> the gift card.</span> 
+      </div>
+    </div>  
+    )
+}
+export default Used;
---
+++ e-commerce/src/won.js
@@ -0,0 +1,40 @@
+
+
+function Won(){
+    return(
+        <div style={{maxWidth:"400px",margin:"auto",backgroundColor:"white"}}>
+      <div style={{height:"300px",maxWidth:"400px",margin:"auto",backgroundColor:"black",color:"white",marginTop:"10px",position:"relative"}}>
+        <div style={{textAlign:"center",fontSize:"30px",paddingTop:"20px"}}>man</div>
+        <div style={{textAlign:"center",fontSize:"30px",paddingLeft:"70px",marginTop:"-15px"}}>matters</div>
+        <div style={{color:"pink",textAlign:"center",fontSize:"28px",marginTop:"30px"}}>Congratulations <img src="https://cdn-icons.flaticon.com/png/128/2274/premium/2274543.png?token=exp=1641393289~hmac=42a46c313a58dff58cae3bd1410d6425" alt="img" height="25px"/></div>
+      </div>
+      <div style={{height:"200px",width:"340px",textAlign:"center",position:"absolute",top:"160px",left:"600px",backgroundColor:"white",marginTop:"20px",borderRadius:"25px",boxShadow:" 2px 5px #888888"}}>
+        <div style={{marginTop:"30px",color:"blue",marginBottom:"30px",fontSize:"25px"}}>pay<span style={{color:"orange"}}>tm</span></div>
+        <div style={{fontSize:"25px",color:"pink"}}>Gift Card</div>
+        <div style={{fontSize:"25px",color:"pink"}}>Of</div>
+        <div style={{fontSize:"35px",color:"blue"}} >Rs 200</div>
+      </div>
+      <div style={{textAlign:"center",fontSize:"20px",marginTop:"85px"}}>You have WON Paytm Gift<br/>Card of Rs 200 for <span style={{fontWeight:"bold"}}>placing the order<br/>on Man Matters</span></div>
+      <div style={{display:"flex",alignItems:"center",marginTop:"20px",justifyContent:"space-evenly"}}>
+        <div><img src="https://cdn-icons-png.flaticon.com/128/220/220236.png" alt="img" height="40px"/></div>
+        <div><span style={{fontWeight:"bold",marginBottom:"0px"}}>Where will i get the gift card?</span><br/>
+        You will receive it on Whatsapp/SMS <br/> on your mobile
+        </div>
+      </div>
+      <div style={{display:"flex",alignItems:"center",marginTop:"20px",justifyContent:"space-evenly"}}>
+        <div><img src="https://cdn-icons-png.flaticon.com/128/2611/2611195.png" alt="img" height="40px"/></div>
+        <div><span style={{fontWeight:"bold",marginBottom:"0px"}}>When will i receive the gift card?</span><br/>
+        Within 48 hours, when your Man <br/>  Matters order is delivered.
+        </div>
+      </div>
+      <div style={{display:"flex",alignItems:"center",marginTop:"20px",justifyContent:"space-evenly"}}>
+        <div><img src="https://cdn-icons-png.flaticon.com/128/1828/1828843.png" alt="img" height="40px"/></div>
+        <div><span style={{fontWeight:"bold",marginBottom:"0px"}}>What if i cancel the order?</span><br/>
+        The voucher will not delivered if you  <br/> cancel the  order.
+        </div>
+      </div>
+      <div style={{textAlign:"center",fontSize:"10px",marginTop:"30px"}}>Powered by <img src="https://th.bing.com/th/id/OIP.46DYDN6HAnhvb3OPC1yewwHaCQ?pid=ImgDet&rs=1" alt="img" height="20px"/></div>
+    </div>
+    )
+}
+export default Won;
